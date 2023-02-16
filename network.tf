6bdbf2b4361e2fef928179a69882046d5c63f679
Alvaro Marcotrigiano <alvaroml@hotmail.com>
2023-02-16 20:15:29 +0000

    Fixed name of network module - 7

--- network.tf
+++ network.tf
@@ -2,10 +2,5 @@ module "network" {
   source  = "app.terraform.io/alvaroml-test-org/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "gaurav_vn"
-}
-
-resource "azurerm_resource_group" "gaurav_vn" {
-  name     = "gaurav-workshop"
-  location = "canadacentral"
+  resource_group_name = "${var.prefix}-workshop"
 }
